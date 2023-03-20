26f878c9666095bd1dbeff9ecabb51c796856964
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-03-20 14:18:42 +0100

    Removed uncessary global variable

--- src/main.c
+++ src/main.c
@@ -1,28 +1,20 @@
 #include "woody.h"
 
-int		g_bin_size;
-
-static void	usage(void)
-{
-	dprintf(STDERR_FILENO, "Usage: ./woody_woodpacker [ELF64 FILE]\n");
-	exit(ERROR);
-}
-
-static void	create_woody(void *fmap, const int bin_size)
+static void	create_woody(void *fmap, const int fsize)
 {
 	int	fd;
 
 	fd = open(WOODY, O_TRUNC | O_CREAT | O_WRONLY, S_IRWXU);
 	if (!fd) {
-		munmap(fmap, g_bin_size);	
+		munmap(fmap, fsize);	
 		dprintf(STDERR_FILENO, "open: %s\n", strerror(errno));
 		exit(ERROR);
 	}
 
 	//map fmap to woody file
-	if (write(fd, fmap, bin_size) == ERROR) {
+	if (write(fd, fmap, fsize) == ERROR) {
 		close(fd);
-		munmap(fmap, g_bin_size);
+		munmap(fmap, fsize);
 		dprintf(STDERR_FILENO, "write: %s\n", strerror(errno));
 		exit(ERROR);
 	}
@@ -30,35 +22,36 @@ static void	create_woody(void *fmap, const int bin_size)
 	close(fd);
 }
 
-static void		*mapbin(char *bin, int *bin_size)
+static void		*mapbin(char *bin, int *fsize)
 {
 	void		*map;
 	int			fd;
 
 	fd = open(bin, O_RDONLY);
-	if (fd == ERROR) {
+	if (fd == ERROR)
+	{
 		dprintf(STDERR_FILENO, "open: %s\n", strerror(errno));	
 		exit(ERROR);
 	}
 
-	//calculate size of bin
-	*bin_size = lseek(fd, 0, SEEK_END);
-	if (*bin_size == ERROR)
+	//Calculate size of binary
+	*fsize = lseek(fd, 0, SEEK_END);
+	if (*fsize == ERROR)
 	{
 		close(fd);
 		dprintf(STDERR_FILENO, "lseek: %s\n", strerror(errno));
 		exit(ERROR);
 	}
 
-	if (*bin_size == 0)
+	if (*fsize == 0)
 	{
 		close(fd);
 		dprintf(STDERR_FILENO, "Invalid size for binary file\n");
 		exit(ERROR);
 	}
 
-	//mmap the bin into map
-	map = mmap(NULL, *bin_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+	//Map binary into memory
+	map = mmap(NULL, *fsize, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
 	if (map == MAP_FAILED)
 	{
 		close(fd);
@@ -74,7 +67,7 @@ static int		is_ELF64(void *fmap)
 {
 	Elf64_Ehdr	*eheader;
 
-	eheader = fmap;
+	eheader = (Elf64_Ehdr *)fmap;
 	if (memcmp(ELFMAG, eheader->e_ident, ELFMAGSIZE) == 0 && (int)(eheader->e_ident[EI_CLASS]) == ELFCLASS64)
 		return 1;
 	return 0;
@@ -83,16 +76,26 @@ static int		is_ELF64(void *fmap)
 int		main(int ac, char **av)
 {
 	void	*fmap;
+	int		fsize;
 
 	if (ac != 2)
-		usage();	
-	fmap = mapbin(av[1], &g_bin_size);
-	if (!is_ELF64(fmap)) {
-		munmap(fmap, g_bin_size);
+	{
+		dprintf(STDERR_FILENO, "Usage: ./woody_woodpacker [ELF64 FILE]\n");
+		return (ERROR);
+	}
+	
+	//Map file into memory
+	fmap = mapbin(av[1], &fsize);
+	if (!is_ELF64(fmap))
+	{
+		munmap(fmap, fsize);
 		dprintf(STDERR_FILENO, "%s not a ELF64 file\n", av[1]);
 		return ERROR;
 	}
+
 	payload_injection(fmap);
-	create_woody(fmap, g_bin_size);
+	
+	create_woody(fmap, fsize);
+	
 	return SUCCESS;
 }
