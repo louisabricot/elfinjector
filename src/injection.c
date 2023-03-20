0de1901f88476f7521dc376c300d2daca0ef64dc
Louisa Malki-Haegel <malkilouisa@protonmail.com>
2023-03-20 18:15:42 +0100

    Trying to add jmp addr into payload data but not working yet

--- src/injection.c
+++ src/injection.c
@@ -1,55 +1,4 @@
 #include "../inc/woody.h"
-# define BASE_ADDRESS ((unsigned char *)0x400000)
-
-uint64_t	g_stub_entrypoint;
-
-void	pt_load_injection(void *fmap, uint64_t old_entry, Elf64_Ehdr *eheader, Elf64_Phdr *pheader, Elf64_Shdr *s)
-{
-	void	*loader;
-	int		jmp_addr;
-	char	jmp = JMP_OPCODE;
-	
-	// Find page size for this system
-	size_t pagesize = sysconf(_SC_PAGESIZE);
-	
-	// Calculate start and end addresses of .text section
-	uintptr_t start = (uintptr_t)(BASE_ADDRESS + s->sh_offset);
-	uintptr_t end = start + s->sh_size;
-
-	// Calculate start of page for mprotect
-	uintptr_t pagestart = start & -pagesize;
-
-	//Calculate the offset to our payload
-	g_stub_entrypoint = pheader->p_offset + pheader->p_memsz;
-
-	//Rewrite the entrypoint to jump to the start of our stub
-	eheader->e_entry = pheader->p_vaddr + pheader->p_memsz + MSG_SIZE;
-
-	//Point to the start of our payload in memory
-	loader = fmap + g_stub_entrypoint;
-
-	//Copy our payload in the padding
-	memcpy(loader, _payload, PAYLOAD_SIZE);
-
-	//Rewrite our payload to use correct addresses
-	memcpy(loader, &pagestart, sizeof(uintptr_t));
-	memcpy(loader + sizeof(uintptr_t), &end, sizeof(uintptr_t));
-	memcpy(loader + sizeof(uintptr_t) + sizeof(uintptr_t), &start, sizeof(uintptr_t));;
-	memcpy(loader + sizeof(uintptr_t) + sizeof(uintptr_t) + sizeof(uintptr_t), &s->sh_size, sizeof(s->sh_size));;
-
-	//Jumping to the end of our payload
-	loader += PAYLOAD_SIZE;
-
-	//Add at the end of the payload a jump instruction to the original entrypoint
-	memcpy(loader, &jmp, sizeof(jmp));
-	loader += sizeof(jmp);
-	jmp_addr = old_entry - (pheader->p_vaddr + pheader->p_memsz + TOTAL_PAYLOAD_SIZE);
-	memcpy(loader, &jmp_addr, sizeof(uint64_t));
-
-	//Update segment size variables with TOTAL_PAYLOAD_SIZE
-	pheader->p_filesz += TOTAL_PAYLOAD_SIZE;
-	pheader->p_memsz += TOTAL_PAYLOAD_SIZE;
-}
 
 static char		*get_name(void *fmap, uint64_t sh_name)
 {
@@ -77,66 +26,103 @@ static Elf64_Shdr	*find_section(void *fmap, char *name)
 	{
 		sh_name = get_name(fmap, sheader[i].sh_name);
 		if (strcmp(name, sh_name) == 0)
-		{
 			return &(sheader[i]);
-		}
-
 	}
 	return NULL;
 }
 
-int	attempt_injection(void *fmap, Elf64_Ehdr *eheader, Elf64_Phdr *pheader)
+static int			pt_load_injection(void *fmap, Elf64_Phdr *pheader, Elf64_Shdr *text)
 {
-	Elf64_Phdr	*next;
-	Elf64_Off	padding_size;
-	Elf64_Xword	old_memsz;
-	Elf64_Shdr	*s;
-	next = pheader + 1;
-	s = find_section(fmap, ".text");
-	if ((pheader->p_flags & PF_X) && (pheader->p_flags & PF_R))
-	{
-		padding_size = pheader->p_offset + pheader->p_filesz - next->p_offset;
-		if (next->p_type == PT_LOAD && padding_size > (Elf64_Off)TOTAL_PAYLOAD_SIZE)
-		{
-			old_memsz = pheader->p_memsz;	
-			pt_load_injection(fmap, eheader->e_entry, eheader, pheader, s);
-			encrypt(fmap + s->sh_offset, s->sh_size);
-			return (1);
-		}
-	}
-	return (0);       
+	Elf64_Ehdr		*eheader;
+	void			*loader;
+	uint64_t		jmp_addr;
+	size_t			page_size;
+	uintptr_t		start;
+	uintptr_t		end;
+	uintptr_t		page_start;
+
+	eheader = (Elf64_Ehdr *)fmap;
+
+	jmp_addr = eheader->e_entry - (pheader->p_vaddr + pheader->p_memsz + PAYLOAD_SIZE);
+	eheader->e_entry = pheader->p_vaddr + pheader->p_memsz + DATA_SIZE;
+
+	// Find page size for this system
+	page_size = sysconf(_SC_PAGESIZE);
+
+	// Calculate start and end of the .text section
+	start = (uintptr_t)(BASE_ADDRESS + text->sh_offset);
+	end = start + text->sh_offset;
+
+	// Calculate start of page for mprotect
+	page_start = start & -page_size;
+
+	//Copy payload into padding
+	loader = fmap + pheader->p_offset + pheader->p_memsz;
+	memcpy(loader, _payload, PAYLOAD_SIZE);
+
+	//Rewrite our payload with correct addresses
+	memcpy(loader, &page_start, sizeof(uintptr_t));
+	memcpy(loader + sizeof(uintptr_t), &end, sizeof(uintptr_t));
+	memcpy(loader + 2 * sizeof(uintptr_t), &start, sizeof(uintptr_t));
+	memcpy(loader + 3 * sizeof(uintptr_t), &text->sh_size, sizeof(uintptr_t));
+	memcpy(loader + 4 * sizeof(uintptr_t), &jmp_addr, sizeof(uint64_t));
+
+	//Update segment size variable with our payload size
+	pheader->p_filesz += PAYLOAD_SIZE;
+	pheader->p_memsz += PAYLOAD_SIZE;
+
+	return (SUCCESS);
 }
 
-void	check_magic_number(Elf64_Ehdr *eheader)
+static int			enough_space(Elf64_Phdr *pheader, Elf64_Phdr *next)
 {
-	int magic = 0xCAFEBABE;
-	if (memcmp(&(eheader->e_ident[EI_PAD]), &magic, sizeof(int)) == 0) {
-		dprintf(STDOUT_FILENO, "Already injected\n");
-		//TODO: does not inject again
-	} else {
-		dprintf(STDOUT_FILENO, "Not injected yet\n");
-	}
+	uint64_t padding_size;
+
+	//TODO: why is padding size computed this way?
+	padding_size = pheader->p_offset + pheader->p_filesz - next->p_offset;
+	if (padding_size > (uint64_t)PAYLOAD_SIZE)
+		return (1);
+	return (0);
 }
 
-int	payload_injection(void *fmap)
+static int			is_injectable(Elf64_Phdr *pheader)
 {
-	Elf64_Ehdr	*eheader;
-	Elf64_Phdr 	*pheader;
+	Elf64_Phdr *next;
 
-	eheader = fmap;
-	pheader = fmap + eheader->e_phoff;
-	check_magic_number(eheader);
-	for (int i = 0; i < eheader->e_phnum; i++ )
+	next = pheader;
+	next++;
+	if (pheader->p_type == PT_LOAD && \
+		(pheader->p_flags & PF_X) && \
+		(pheader->p_flags & PF_R) && \
+		next->p_type == PT_LOAD && \
+		enough_space(pheader, next))
+		return (1);
+	return (0);
+}
+
+int					inject(void *fmap)
+{
+	Elf64_Ehdr		*eheader;
+	Elf64_Phdr		*pcurr;
+	Elf64_Shdr		*text;
+
+	eheader = (Elf64_Ehdr *)fmap;
+	pcurr = (Elf64_Phdr *)(fmap + eheader->e_phoff);
+	for (int i = 1; i < eheader->e_phnum; i++ )
 	{
-		if (pheader->p_type == PT_LOAD)
+		if (is_injectable(pcurr))
 		{
-			if (attempt_injection(fmap, eheader, pheader)) {
-				//TODO: print OXCAFEBABE in padding
-				dprintf(STDOUT_FILENO, "Injected\n");	
+			printf("Is injectable\n");
+			text = find_section(fmap, ".text");
+			if (pt_load_injection(fmap, pcurr, text) == SUCCESS)
+			{
+				encrypt(fmap + text->sh_offset, text->sh_size);
 				return (SUCCESS);
 			}
+			//TODO: print OXCAFEBABE in padding
 		}
-		pheader++;
+		pcurr++;
 	}
-	return ERROR;
+	printf("Could not inject!");
+	return (ERROR);
 }
--- src/payload.s
+++ src/payload.s
@@ -5,10 +5,13 @@ SECTION .text
 	global _decrypt
 
 _payload:
-pagestart dq 0x00000000000000001
-end dq 0x0000000000000000001
-textstart dq  0x0000000000000001
-textlen dq	0x0000000000000001
+
+pagestart dq 0xffffffffffffffff
+end dq 0xffffffffffffffff
+textstart dq  0xffffffffffffffff
+textlen dq	0xffffffffffffffff
+address dq	0x0000000000000000
+
 msg db `.....WOODY.....\n`, 0x0
 
 _stub:
@@ -19,14 +22,14 @@ _stub:
 	mov rax, 1					 
 	syscall
 _writeable:
-	;mov rdi, 0
-	;mov rsi, 0
 	mov rdi, [rel pagestart]
 	mov rsi, [rel end]
 	sub rsi, [rel pagestart]
 	mov rdx, 0x7
 	mov rax, 10
 	syscall
+
+_decrypt:
 	mov rdi, [rel textstart]
 	mov rdx, [rel textlen]
 .decrypt_loop:
@@ -41,12 +44,16 @@ _writeable:
 	inc rdi
 	dec rdx
 	jmp .decrypt_loop
+
 _mprotect:
-;	mov rdi, [rel pagestart] 
-;	mov rsi, [rel end]
-;	sub rsi, [rel pagestart]
-;	mov rdx, 0x5
-;	mov rax, 10
-;	syscall
+	mov rdi, [rel pagestart] 
+	mov rsi, [rel end]
+	sub rsi, [rel pagestart]
+	mov rdx, 0x5
+	mov rax, 10
+	syscall
 	pop rdx
+_jmp_to_old_entrypoint:
+	mov rax, [rel address]
+	jmp rax
 _payloadend:
